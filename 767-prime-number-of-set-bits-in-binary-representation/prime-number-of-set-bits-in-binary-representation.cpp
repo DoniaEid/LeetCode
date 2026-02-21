22d168531d3b815120756478ba2bb27a78c40c42
Donia Eid <doniaeid815@gmail.com>
2026-02-21 11:12:56 +0200

    Time: 23 ms (29.15%) | Memory: 8.4 MB (13.87%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
-        int count=0;
-        for(int i=left;i<=right;i++){
-            int c=binary(i);
-            if(prime(c)){
-                count++;
+        int count = 0;
+        unordered_set<int> primes = {2,3,5,7,11,13,17,19};
+        for(int i=left;i<=right;++i){
+            int num = i;
+            int bits = 0;
+            while(num){
+                if(num&1){
+                    ++bits;
+                }
+                num>>=1;
+            }
+            if(primes.find(bits)!=primes.end()){
+                ++count;
             }
         }
         return count;
     }
-    bool prime(int num){
-        if(num<=1){
-            return false;
-        }
-        bool found=true;
-        for(int i=2;i*i<=num;i++){
-          if(num%i==0){
-            found=false;
-            break;
-          }
-        }
-        return found;
-    }
-    int binary(int num){
-        string s="";
-        int c=0;
-        while(num!=0){
-            s+=(num%2)+'0';
-            num/=2;
-        }
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='1'){
-                   c++;
-            }
-        }
-        return c;
-    }
-
-
 };
