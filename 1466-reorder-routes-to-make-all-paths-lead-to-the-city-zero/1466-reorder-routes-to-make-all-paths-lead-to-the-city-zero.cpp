1f5c3cfc484b112fa371b8aee4052c5f8ab25bd3
Donia Eid <doniaeid815@gmail.com>
2026-02-21 12:04:56 +0200

    Time: 96 ms (66.81%), Space: 106 MB (74.67%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int count = 0;
 
-    void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj) {
-        for (auto& neighbor : adj[node]) {
-            int child = neighbor.first;
-            int sign = neighbor.second;
+    void solve(int node, int parent, vector<vector<pair<int, int>>>& adj) {
+        for (auto& edge : adj[node]) {
+            int neighbor = edge.first;
+            int isOriginal = edge.second;
 
-            if (child != parent) {
-                count += sign;
-                dfs(child, node, adj);
+            if (neighbor != parent) {
+                if (isOriginal == 1) {
+                    count++;
+                }
+                solve(neighbor, node, adj);
             }
         }
     }
 
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int, int>>> adj(n);
-        count = 0; 
-
-        for (auto& edge : connections) {
-            adj[edge[0]].push_back({edge[1], 1});
-            adj[edge[1]].push_back({edge[0], 0});
+        for (auto& c : connections) {
+            adj[c[0]].push_back({c[1], 1}); 
+            adj[c[1]].push_back({c[0], 0}); 
         }
 
-        dfs(0, -1, adj);
-        
+        count = 0;
+        solve(0, -1, adj);
+
         return count;
     }
 };
