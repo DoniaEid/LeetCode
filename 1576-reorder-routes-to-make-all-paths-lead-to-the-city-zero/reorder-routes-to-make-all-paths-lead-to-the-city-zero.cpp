bdeaabeb53a43d0399aa9ed4f318ae329e548006
Donia Eid <doniaeid815@gmail.com>
2026-02-21 11:37:25 +0200

    Time: 88 ms (80.75%) | Memory: 106.1 MB (69.75%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<pair<int, bool>>> adj(n);
-        for (auto& edge : connections) {
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back({v, true});
-            adj[v].push_back({u, false});
-        }
+    int count = 0;
 
-        int count = 0;
-        vector<bool> visited(n, false);
-        queue<int> q;
+    void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj) {
+        for (auto& neighbor : adj[node]) {
+            int child = neighbor.first;
+            int sign = neighbor.second;
 
-        q.push(0); 
-        visited[0] = true;
-
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
-
-            for (auto& neighbor : adj[curr]) {
-                int nextNode = neighbor.first;
-                bool isOriginal = neighbor.second;
-
-                if (!visited[nextNode]) {
-                    if (isOriginal) {
-                        count++;
-                    }
-                    visited[nextNode] = true;
-                    q.push(nextNode);
-                }
+            if (child != parent) {
+                count += sign;
+                dfs(child, node, adj);
             }
         }
+    }
 
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int, int>>> adj(n);
+        count = 0; 
+
+        for (auto& edge : connections) {
+            adj[edge[0]].push_back({edge[1], 1});
+            adj[edge[1]].push_back({edge[0], 0});
+        }
+
+        dfs(0, -1, adj);
+        
         return count;
     }
 };
