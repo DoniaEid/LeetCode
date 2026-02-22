07a151c29b6c164bbd5ed7217333fc470031c936
Donia Eid <doniaeid815@gmail.com>
2026-02-22 20:18:31 +0200

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.75%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int binaryGap(int n) {
-       string s= binary(n);
-       int m=0;
-       for(int i=0;i<s.size();i++){
-          if(s[i]=='1'){
-            for(int j=i+1;j<s.size();j++){
-              if(s[j]=='1'){
-                    m=max(m,j-i);
-                    break;
-               }
-                  
-            }
-          }
-       }
-       return m;
-    }
-    string binary(int n){
-        string t="";
-        while(n!=0){
-            t+=(n%2)+'0';
-            n/=2;
-        }
-        return t;
-    }
+        int max_gap = 0;
+        int last_pos = -1; 
+        int current_pos = 0;
 
+        while (n > 0) {
+            if (n % 2 == 1) {
+                if (last_pos != -1) {
+                    max_gap = max(max_gap, current_pos - last_pos);
+                }
+                last_pos = current_pos;
+            }
+            n /= 2;
+            current_pos++;
+        }
+
+        return max_gap;
+    }
 };
