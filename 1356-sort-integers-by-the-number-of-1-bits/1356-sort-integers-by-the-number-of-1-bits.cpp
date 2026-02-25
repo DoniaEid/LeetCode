47ead22012dbbb791bce2ac54e3cad931c7449fd
Donia Eid <doniaeid815@gmail.com>
2026-02-25 16:22:23 +0200

    Time: 5 ms (23.28%), Space: 17.7 MB (5.1%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int binary(int num) {
-        int count = 0;
-        int temp = num; 
-        while (temp != 0) {
-            if (temp % 2 == 1) count++;
-            temp /= 2;
-        }
-        return count;
-    }
-
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int, int>> helper;
-
-        for (int i = 0; i < arr.size(); i++) {
-            int bits = binary(arr[i]);
-            helper.push_back({bits, arr[i]});
+        map<int, multiset<int>> bitmap;
+        for(int val : arr) {
+            int bits = __builtin_popcount(val);
+            bitmap[bits].insert(val);
         }
-
-        sort(helper.begin(), helper.end());
-
-        vector<int> result;
-        for (int i = 0; i < helper.size(); i++) {
-            result.push_back(helper[i].second); 
-        }
-
-        return result;
+        
+        vector<int> ans;
+        for(auto p : bitmap) {
+            for(int val : p.second) {
+                ans.push_back(val);
+            }
+        } 
+        return ans;
     }
 };
