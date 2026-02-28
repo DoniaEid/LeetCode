2a268bde96886c599de480568be7ca795cecb0ab
Donia Eid <doniaeid815@gmail.com>
2026-02-28 11:35:31 +0200

    Time: 26 ms (77.85%), Space: 8.1 MB (41.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -7,17 +7,14 @@ public:
         const int MOD = 1e9 + 7;
         long long result = 0;
         int length = 0; 
-        int nextPower = 1;
+        long long pow2 = 1;
+        int nextPower = 1; 
 
         for (int i = 1; i <= n; i++) {
             if (i == nextPower) {
                 length++;
-                nextPower *= 2;
-            }
-
-            long long pow2 = 1;
-            for (int j = 0; j < length; j++) {
                 pow2 = (pow2 * 2) % MOD;
+                nextPower *= 2;
             }
 
             result = (result * pow2 + i) % MOD;
@@ -26,5 +23,3 @@ public:
         return result;
     }
 };
-
-
