d2a3f1ec27006e4d1c3d864dec141b2200c37c26
Donia Eid <doniaeid815@gmail.com>
2026-06-22 12:06:25 +0300

    Time: 3 ms (12.33%), Space: 9 MB (39.61%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int count_b=0;
-        int count_o=0;
-        int count_a=0;
-        int count_l=0;
-        int count_n=0;
+       map<char,int>freq;
         for(int i=0;i<text.size();i++){
-             if(text[i]=='a'){
-                count_a++;
-             }
-             if(text[i]=='b'){
-                count_b++;
-             }
-             if(text[i]=='l'){
-                count_l++;
-             }
-             if(text[i]=='o'){
-                count_o++;
-             }
-             if(text[i]=='n'){
-                count_n++;
-             }
+            freq[text[i]]++;
         }
-        count_l/=2;
-        count_o/=2;
+        freq['l']/=2;
+        freq['o']/=2;
 
         return min({
-            count_a,
-            count_b,
-            count_l,
-            count_o,
-            count_n
+            freq['a'],
+            freq['b'],
+            freq['l'],
+            freq['o'],
+            freq['n']
         });
  
     }
